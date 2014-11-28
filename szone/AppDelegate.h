07c59a448b4a4f7bc608d026e5672ecc801e8dab
icargo <icargo@icloud.com>
2014-11-28 10:38:27 +0800

    Initial Commit

---
+++ szone/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  szone
+//
+//  Created by zhuyq-MacBook Pro on 14/11/28.
+//  Copyright (c) 2014年 lewei.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ szone/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  szone
+//
+//  Created by zhuyq-MacBook Pro on 14/11/28.
+//  Copyright (c) 2014年 lewei.com. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
